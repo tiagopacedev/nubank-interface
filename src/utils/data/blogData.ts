f167eda8468af8723821e476b145aa1e5a59f17a
Tiago Pace <tiagopace.dev@gmail.com>
2024-02-21 00:43:51 -0300

    feat(data): Enhance blogData with new titles, descriptions, and id props, and utilize require for image imports
    This commit improves the blogData by adding new titles and descriptions for enhanced information. It also introduces the id property to uniquely identify each item in the data. Additionally, the commit utilizes the require function for importing images.

--- src/utils/data/blogData.ts
+++ src/utils/data/blogData.ts
@@ -1,26 +1,39 @@
-// Certifique-se de que o caminho está correto
-import AppImage from '../../../assets/app.jpeg';
+const AppImage = require('../../../assets/app.jpeg');
+const BankImage = require('../../../assets/bank.jpeg');
+const CardImage = require('../../../assets/card.webp');
+const PeopleImage = require('../../../assets/people.jpeg');
 
 export const blogData = [
   {
+    id: 1,
     img: AppImage,
     title: 'Nubank Celular Seguro',
     description: '100% cobertura, 0% estresse. Simule agora mesmo.',
     buttonText: 'Conhecer',
-    link: '',
+    link: 'https://nubank.com.br/',
   },
   {
-    img: AppImage,
-    title: 'Nubank Celular Seguro',
-    description: '100% cobertura, 0% estresse. Simule agora mesmo.',
+    id: 2,
+    img: CardImage,
+    title: 'Cartão de Crédito Nubank',
+    description: 'Desfrute de benefícios exclusivos e zero anuidade.',
     buttonText: 'Conhecer',
-    link: '',
+    link: 'https://nubank.com.br/',
   },
   {
-    img: AppImage,
-    title: 'Nubank Celular Seguro',
-    description: '100% cobertura, 0% estresse. Simule agora mesmo.',
+    id: 3,
+    img: PeopleImage,
+    title: 'Indique o Nu para amigos',
+    description: 'Espalhe como é simples estar no controle.',
+    buttonText: 'Indicar',
+    link: 'https://nubank.com.br/',
+  },
+  {
+    id: 4,
+    img: BankImage,
+    title: 'Termos de uso',
+    description: 'Explicamos o que diz esse documento do Nubank.',
     buttonText: 'Conhecer',
-    link: '',
+    link: 'https://nubank.com.br/',
   },
 ];
