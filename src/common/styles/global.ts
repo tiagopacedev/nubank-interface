d6e364d96710366482d84d8dccb67cb644ccb567
Tiago Pace <tiagopace.dev@gmail.com>
2024-02-21 00:26:52 -0300

    feat(styles/global): implement global styles to the app

---
+++ src/common/styles/global.ts
@@ -0,0 +1,18 @@
+export const colors = {
+  primary: '#8a05be', // Purple - light mode
+  secondary: '#820AD1', // Purple - dark mode
+  background: '#f5f5f5',
+  lightGray: '#E5E5E5',
+  text: '#333',
+  accent: '#00c86f',
+  error: '#CF0505',
+};
+
+export const fontSizes = {
+  extraLarge: 22,
+  larger: 20,
+  large: 18,
+  medium: 16,
+  small: 14,
+  smallest: 12,
+};
